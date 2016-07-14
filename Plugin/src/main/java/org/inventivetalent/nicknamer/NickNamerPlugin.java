d9c365e57138449ff585d097f572b1568d559069
inventivetalent <inventivetalent@inventivegames.de>
2016-07-14 14:55:38 +0200

    add java version check

--- Plugin/src/main/java/org/inventivetalent/nicknamer/NickNamerPlugin.java
+++ Plugin/src/main/java/org/inventivetalent/nicknamer/NickNamerPlugin.java
@@ -149,6 +149,14 @@ public class NickNamerPlugin extends JavaPlugin implements Listener, PluginMessa
 
 	@Override
 	public void onLoad() {
+		String javaVersion = System.getProperty("java.version");
+		getLogger().info("Java Version: " + javaVersion);
+		int majorVersion = Integer.parseInt(javaVersion.split("\\.")[1]);
+		if (majorVersion < 8) {
+			getLogger().severe("Please use Java 8 or higher (is " + majorVersion + ")");
+			throw new RuntimeException("NickNamer requires Java 8+");
+		}
+
 		APIManager.require(PacketListenerAPI.class, this);
 		APIManager.registerAPI(new NickNamerAPI(), this);
 	}
