a18a579ab71eba376864919b4c5745b20d056e62
inventivetalent <mail@inventivetalent.org>
2020-04-08 16:18:00 +0200

    add support for offline command targets, as suggested in #111

--- Plugin/src/main/java/org/inventivetalent/nicknamer/command/CommandUtil.java
+++ Plugin/src/main/java/org/inventivetalent/nicknamer/command/CommandUtil.java
@@ -28,7 +28,9 @@
 
 package org.inventivetalent.nicknamer.command;
 
+import com.google.common.base.Charsets;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.inventivetalent.nicknamer.NickNamerPlugin;
@@ -36,8 +38,22 @@ import org.inventivetalent.pluginannotations.PluginAnnotations;
 import org.inventivetalent.pluginannotations.command.exception.InvalidLengthException;
 import org.inventivetalent.pluginannotations.message.MessageLoader;
 
+import java.util.UUID;
+
 public class CommandUtil {
 	static MessageLoader MESSAGE_LOADER = PluginAnnotations.MESSAGE.newMessageLoader(NickNamerPlugin.instance, "config.yml", "messages.command", null);
+
+	static TargetInfo findTargetInfo(CommandSender sender, String targetName, boolean otherTarget, boolean allowOffline) {
+		if (allowOffline) {
+			return findOfflineTargetInfo(sender, targetName, otherTarget);
+		} else {
+			Player player = findTarget(sender, targetName, otherTarget);
+			if (player != null) {
+				return new TargetInfo(player);
+			}
+		}
+		return null;
+	}
 
 	static Player findTarget(CommandSender sender, String targetName, boolean otherTarget) {
 		Player target;
@@ -55,6 +71,92 @@ public class CommandUtil {
 			return null;
 		}
 		return target;
+	}
+
+	static UUID findOfflineTargetUUID(CommandSender sender, String targetName, boolean otherTarget) {
+		UUID uuid = null;
+		if (otherTarget) {
+			Player target = Bukkit.getPlayer(targetName);
+			if (target != null) {
+				uuid = target.getUniqueId();
+			} else {
+				try {
+					// Paper
+					uuid = Bukkit.getPlayerUniqueId(targetName);
+				} catch (Exception ignored) {
+					// Spigot
+					OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(targetName);
+					UUID offlineUuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + targetName).getBytes(Charsets.UTF_8));
+					if (!offlineUuid.equals(offlinePlayer.getUniqueId())) { // We want the real UUID, not the one generated from OfflinePlayer:name
+						uuid = offlinePlayer.getUniqueId();
+					}
+				}
+			}
+		} else {
+			if (sender instanceof Player) {
+				uuid = ((Player) sender).getUniqueId();
+			} else {
+				throw new InvalidLengthException(2, 1);
+			}
+		}
+		if (uuid == null) {
+			sender.sendMessage(MESSAGE_LOADER.getMessage("error.target.notFound", "error.target.notFound"));
+			return null;
+		}
+		return uuid;
+	}
+
+	static TargetInfo findOfflineTargetInfo(CommandSender sender, String targetName, boolean otherTarget) {
+		TargetInfo targetInfo = null;
+		if (otherTarget) {
+			Player target = Bukkit.getPlayer(targetName);
+			if (target != null) {
+				targetInfo = new TargetInfo(target);
+			} else {
+				sender.sendMessage(MESSAGE_LOADER.getMessage("offlineInfo", "&eThe requested player is currently offline. Changes will be applied when they come online."));
+				try {
+					// Paper
+					targetInfo = new TargetInfo(Bukkit.getPlayerUniqueId(targetName), targetName);
+				} catch (Exception ignored) {
+					// Spigot
+					OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(targetName);
+					UUID offlineUuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + targetName).getBytes(Charsets.UTF_8));
+					if (!offlineUuid.equals(offlinePlayer.getUniqueId())) { // We want the real UUID, not the one generated from OfflinePlayer:name
+						targetInfo = new TargetInfo(offlinePlayer);
+					}
+				}
+			}
+		} else {
+			if (sender instanceof Player) {
+				targetInfo = new TargetInfo((Player) sender);
+			} else {
+				throw new InvalidLengthException(2, 1);
+			}
+		}
+		if (targetInfo == null) {
+			sender.sendMessage(MESSAGE_LOADER.getMessage("error.target.notFound", "error.target.notFound"));
+			return null;
+		}
+		return targetInfo;
+	}
+
+
+	public static class TargetInfo {
+		UUID uuid;
+		String name;
+
+		public TargetInfo(UUID uuid, String name) {
+			this.uuid = uuid;
+			this.name = name;
+		}
+
+		public TargetInfo(OfflinePlayer player) {
+			if (player != null) {
+				this.uuid = player.getUniqueId();
+				this.name = player.getName();
+			}
+		}
+
 	}
 
 }
